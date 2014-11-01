3a48aa31a66c27fb23fe733c3ec429aab45a617d
Daniel Kazak <daniel.kazak@gmail.com>
2014-11-02 01:13:19 +0200

    Added descriptions

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,20 +9,18 @@
  */
 angular.module('shaibaApp')
   .controller('MainCtrl', function ($scope, parse) {
+
+        // Define scope items for sentence
         $scope.dish = '';
         $scope.nation = '';
         $scope.adj = '';
 
-        $scope.awesomeThings = [
-            'HTML5 Boilerplate',
-            'AngularJS',
-            'Karma'
-        ];
-
+        // Random function
         function Random(num){
             return Math.floor(Math.random() * num);
         }
 
+        // Initialize $scope vars for DOM (check the main.HTML page)
         parse.getParse('dishes').success(function(data){$scope.dish = data.results[Random(data.results.length)].dishName; });
         parse.getParse('nations').success(function(data){$scope.nation = data.results[Random(data.results.length)].natName; });
         parse.getParse('adj').success(function(data){$scope.adj = data.results[Random(data.results.length)].adjName; });
