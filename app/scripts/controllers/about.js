2fc506bcfbbebe2b235fa83c03e80e40c4137ef7
Daniel Kazak <daniel.kazak@gmail.com>
2014-11-02 01:22:15 +0200

    TODO: Settings page

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -9,30 +9,6 @@
  */
 angular.module('shaibaApp')
   .controller('AboutCtrl', function ($scope, parse) {
-
-    $scope.dish;
-    $scope.nation;
-    $scope.adj;
-
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-
-    function Random(num){
-            return Math.floor(Math.random() * num);
-        }
-
-     parse.getParse('dishes').success(function(data){$scope.dish = data.results[Random(data.results.length)].dishName; });
-     parse.getParse('nations').success(function(data){$scope.nation = data.results[Random(data.results.length)].natName; });
-     parse.getParse('adj').success(function(data){$scope.adj = data.results[Random(data.results.length)].adjName; });
-
-
-
-
-
-
-
+        // TO DO
 
   });
