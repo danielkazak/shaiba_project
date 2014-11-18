c4b5515b2de21853b3b91faa5c9014d87199f4c5
Daniel Kazak <daniel.kazak@gmail.com>
2014-11-18 21:14:42 +0200

    Shaiba animation changed

--- app/scripts/controllers/info.js
+++ app/scripts/controllers/info.js
@@ -10,8 +10,6 @@
 angular.module('shaibaApp')
   .controller('InfoCtrl', function ($scope) {
         $scope.doronTitle = 'דורון סגס';
-        $scope.doronContent = 'content';
         $scope.danielTitle = 'דניאל קזק';
-        $scope.danielContent = 'content';
         $scope.mailDoron = 'https://mail.google.com/mail/u/0/?view=cm&fs=1&to=sagesdoron@gmail.com&su=SUBJECT&body=Hey%20Doron,%20&tf=1';
   });
